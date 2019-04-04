7b166b8038a665fe3e95f21b72eccbda00a565b0
khaledhazime <hazimekhaled@outlook.com>
2019-04-04 14:27:06 -0300

    ex1.c

---
+++ 3_exercicios_vetores/ex1.c
@@ -0,0 +1,21 @@
+/*
+1) Fazer um programa contendo funções para:
+
+a) ler um vetor onde cada elemento (vagas[i]) representa o número de vagas do curso i.
+
+b) ler um outro vetor onde cada elemento (candidatos[i]) representa o número de candidatos inscritos para o curso i.
+
+c) criar um terceiro vetor contendo o número de candidatos por vaga
+
+d) receber o índice de um curso e retornar o número de candidatos por vaga desse curso
+
+e) retornar o índice do curso de maior concorrência*/
+int vagas(int *v[]){
+    for(int i=0; i<n; i++){
+
+    }
+}
+int main(){
+    int v[100];
+    vagas(&v);
+}
