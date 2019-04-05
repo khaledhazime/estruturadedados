b018ee6968de9e77d31741c57e557821852eb4ae
khaledhazime <hazimekhaled@outlook.com>
2019-04-05 13:08:22 -0300

    ex1

--- 3_exercicios_vetores/ex1.c
+++ 3_exercicios_vetores/ex1.c
@@ -10,12 +10,53 @@ c) criar um terceiro vetor contendo o número de candidatos por vaga
 d) receber o índice de um curso e retornar o número de candidatos por vaga desse curso
 
 e) retornar o índice do curso de maior concorrência*/
-int vagas(int *v[]){
+#include <stdio.h>
+//Letra a)
+int vagas(int *v, int n){
     for(int i=0; i<n; i++){
-
+        printf("Digite o numero de vagas do curso %d\n", i+1);
+        scanf("%d", &v[i]);
+    }
+}
+//Letra b)
+int candidatos(int *c, int n){
+    for(int i=0; i<n; i++){
+        printf("Digite o numero de candidatos para o curso %d\n", i+1);
+        scanf("%d", &c[i]);
+    }
+}
+//Letra c)
+float calculo(float *cpv, int *c,int *v, int n){
+    for(int i=0; i<n; i++){
+        cpv[i] = (c[i]/v[i]);
+    }
+}
+//Letra d)
+int indice(int i, float *cpv){
+    printf("O numero de candidatos por vaga do curso %d é: %.2f candidatos por vaga\n", i+1, cpv[i]);
+}
+//Letra e)
+int mc(float *cpv, int n){
+    int maior, x;
+    for(int i=0; i<n; i++){
+        if (cpv[i]>maior){
+            maior = i;
+    }
+    return (maior+1);
     }
 }
 int main(){
-    int v[100];
-    vagas(&v);
+    int v[100], c[100], n, i;
+    float cpv[100];
+    printf("Deseja digitar quantos cursos?\n");
+    scanf("%d", &n);
+    vagas(v, n);
+    candidatos(c,n);
+    calculo(cpv, c, v, n);
+    printf("Digite o indíce do curso\n");
+    scanf("%d", &i);
+    i-=i;
+    indice(i, cpv);
+    printf("O curso com maior numero de candidatos por vaga eh o curso %i, que possui %.2f\n", mc(cpv, n), cpv[mc(cpv, n)]);
+    
 }
